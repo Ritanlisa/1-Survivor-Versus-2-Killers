9e23fd740db64124ede5ef4c96b9f245000724e8
Ritanlisa <40826407+Ritanlisa@users.noreply.github.com>
2019-07-25 19:47:01 +0800

    Add files via upload

---
+++ Released Versions And History Releases/History Codes/1SVS2K 2.2.3.1/1SV2K(x64)_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef 1SV2K(X64)_PRIVATE_H
+#define 1SV2K(X64)_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"2.2.2.2"
+#define VER_MAJOR	2
+#define VER_MINOR	2
+#define VER_RELEASE	2
+#define VER_BUILD	2
+#define COMPANY_NAME	"Immortal Rover Games"
+#define FILE_VERSION	"2.2.2.2"
+#define FILE_DESCRIPTION	"Ò»ÐÜÕ½¶þ¹·"
+#define INTERNAL_NAME	"1SV2K"
+#define LEGAL_COPYRIGHT	"Immortal Rover Games Studio"
+#define LEGAL_TRADEMARKS	"Immortal Rover Games Studio"
+#define ORIGINAL_FILENAME	"1SV2K"
+#define PRODUCT_NAME	"1SV2K"
+#define PRODUCT_VERSION	"2.2.2.2"
+
+#endif /*1SV2K(X64)_PRIVATE_H*/

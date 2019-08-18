5842e0a66ac84858a94ea9676c2517fd4bc98525
Ritanlisa.D.Millatiz <40826407+Ritanlisa@users.noreply.github.com>
2019-08-18 20:38:15 +0800

    Add files via upload

--- 1SVS2K-Source/1SV2K(x64)_private.h
+++ 1SVS2K-Source/1SV2K(x64)_private.h
@@ -5,19 +5,19 @@
 #define 1SV2K(X64)_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"2.3.3.3"
+#define VER_STRING	"2.5.1.1"
 #define VER_MAJOR	2
-#define VER_MINOR	3
-#define VER_RELEASE	3
-#define VER_BUILD	3
+#define VER_MINOR	5
+#define VER_RELEASE	1
+#define VER_BUILD	1
 #define COMPANY_NAME	"Immortal Rover Games"
-#define FILE_VERSION	"2.3.3.3"
+#define FILE_VERSION	"2.5.1.1"
 #define FILE_DESCRIPTION	"Ò»ÐÜÕ½¶þ¹·"
 #define INTERNAL_NAME	"1SV2K"
 #define LEGAL_COPYRIGHT	"Immortal Rover Games Studio"
 #define LEGAL_TRADEMARKS	"Immortal Rover Games Studio"
 #define ORIGINAL_FILENAME	"1SV2K"
 #define PRODUCT_NAME	"1SV2K"
-#define PRODUCT_VERSION	"2.3.3.3"
+#define PRODUCT_VERSION	"2.5.1.1"
 
 #endif /*1SV2K(X64)_PRIVATE_H*/
